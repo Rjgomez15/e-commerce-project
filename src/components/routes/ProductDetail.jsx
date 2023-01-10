5b6f57001a1b941803a346cd028460b210079974
Raul Gomez <89232330+Rjgomez15@users.noreply.github.com>
2023-01-10 13:01:48 -0400

    Update ProductDetail.jsx

--- src/components/routes/ProductDetail.jsx
+++ src/components/routes/ProductDetail.jsx
@@ -11,7 +11,7 @@ const ProductDetail = () => {
   const { id } = useParams()
 
   useEffect(() => {
-    const URL = `https://ecommerce-api-react.herokuapp.com/api/v1/products/${id}`
+    const URL = `https://e-commerce-api.academlo.tech/api/v1/products/${id}`
     axios.get(URL)
       .then(res => setProductInfo(res.data.data.product))
       .catch(err => console.log(err))
